aafdd6fdf9da9fa9f4c8f908030b7b161a6345f6
bumie <akinremibunmi111@gmail.com>
2023-11-01 07:07:00 +0100

    Update courses.js

--- docs/courses.js
+++ docs/courses.js
@@ -1,15 +1,45 @@
-// Fetch subtopics from an external API and populate the side navigation
-fetch('YOUR_API_ENDPOINT_HERE')
+// Define the API endpoint and request parameters
+const API_ENDPOINT = "https://innovatex.azurewebsites.net/course";
+const requestData = {
+    "language": "English",
+    "course_code": "CSC401",
+    "page_number": 0
+};
+
+// Fetch course information from the external API and populate the courses.html page
+fetch(API_ENDPOINT, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(requestData)
+})
 .then(response => response.json())
 .then(data => {
-    const subtopicsNav = document.getElementById('subtopics-nav');
-    data.subtopics.forEach(subtopic => {
-        const subtopicLink = document.createElement('a');
-        subtopicLink.href = "#"; // You can replace this with actual links if needed
-        subtopicLink.textContent = subtopic.title;
-        subtopicsNav.appendChild(subtopicLink);
-    });
+    // Check if the response contains the expected data
+    if (data && data.message) {
+        // Populate the course title
+        const courseTitleElement = document.getElementById('course-title');
+        courseTitleElement.textContent = "Course Title: " + data.message["course-title"];
+
+        // Populate the list of subtopics
+        const subtopicsContainer = document.getElementById('subtopics-container');
+        for (let subtopicTitle in data.message.subtopics) {
+            const subtopicDiv = document.createElement('div');
+            const subtopicHeader = document.createElement('h3');
+            const subtopicContent = document.createElement('p');
+
+            subtopicHeader.textContent = subtopicTitle;
+            subtopicContent.textContent = data.message.subtopics[subtopicTitle].indepth_response;
+
+            subtopicDiv.appendChild(subtopicHeader);
+            subtopicDiv.appendChild(subtopicContent);
+            subtopicsContainer.appendChild(subtopicDiv);
+        }
+    } else {
+        console.error('Unexpected API response format:', data);
+    }
 })
 .catch(error => {
-    console.error('Error fetching subtopics:', error);
+    console.error('Error fetching course information:', error);
 });
